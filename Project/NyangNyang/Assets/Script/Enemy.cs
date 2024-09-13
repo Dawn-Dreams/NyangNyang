1dd850cc91c5a3867014c97c39c27bfd8045ae6a
Lee Yoon Seok <kjggr123@gmail.com>
2024-09-14 00:26:29 +0900

    Enemy 프리펩 내 Enemy의 체력에 대한 slider 및 스크립트 추가

--- Project/NyangNyang/Assets/Script/Enemy.cs
+++ Project/NyangNyang/Assets/Script/Enemy.cs
@@ -2,16 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : Character
 {
     public GameObject floatingDamage;
 
+    private Slider _healthSlider;
+    private TextMeshProUGUI _healthText;
 
     protected override void Awake()
     {
         characterID = 1;
         base.Awake();
+
+        _healthSlider = gameObject.GetComponentInChildren<Slider>();
+        _healthText = _healthSlider.GetComponentInChildren<TextMeshProUGUI>();
+
+        _healthSlider.maxValue = status.hp;
+        _healthSlider.minValue = 0;
+        _healthSlider.value = currentHP;
+
+        SetHealthBarText();
     }
 
     protected override bool TakeDamage(int damage)
@@ -23,11 +35,19 @@ public class Enemy : Character
             // 대미지 출력
             GameObject textObject = Instantiate(floatingDamage, transform.position, Quaternion.identity);
             textObject.GetComponentInChildren<TextMesh>().text = " " + damage + " ";
-            
             Destroy(textObject,2.0f);
-            //textObject.GetComponentInChildren<TextMeshProUGUI>().SetText();
         }
+
+        _healthSlider.value = currentHP;
+        SetHealthBarText();
 
         return getDamaged;
     }
+
+    private void SetHealthBarText()
+    {
+        if (_healthText== null) return;
+        _healthText.SetText(_healthSlider.value + " / " + _healthSlider.maxValue);
+    }
 }
+
