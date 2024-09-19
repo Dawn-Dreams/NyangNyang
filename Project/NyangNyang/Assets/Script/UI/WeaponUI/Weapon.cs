066158e5529f3f8da17ee0da34acffb4e9d86e1f
GH_NoteBook <elfgh29@naver.com>
2024-09-19 15:07:26 +0900

    기본 weapon class 및 manager 생성 및 구현

--- Project/NyangNyang/Assets/Script/UI/WeaponUI/Weapon.cs
+++ Project/NyangNyang/Assets/Script/UI/WeaponUI/Weapon.cs
@@ -6,7 +6,37 @@ public class Weapon
     int grade;          // 장비 등급
     int subGrade;       // 세부 등급
 
-    int stats;          // 장비 세부 능력치
+    int status;          // 장비 세부 능력치
     int level;          // 장비 레벨
 
+
+    public Weapon(string _name, int _grade, int _subGrade, int _level, int _possession)
+    {
+        name = _name;
+        possession = _possession;
+        grade = _grade;
+        subGrade = _subGrade;
+        level = _level;
+    }
+
+    public void LevelUP()
+    {
+        // TODO: 레벨업 로직 만들기
+        level++;
+    }
+
+    public void AddWeapon(int count)
+    {
+        possession += count;
+    }
+
+    public bool HasWeapon()
+    {
+        return possession > 0;
+    }
+
+    public int GetWeaponCount()
+    {
+        return possession;
+    }
 }
---
+++ Project/NyangNyang/Assets/Script/UI/WeaponUI/WeaponManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour
+{
+    private Weapon[] weapons = new Weapon[32];
+
+    public void InitializedWeapons()
+    {
+        // TODO: 서버에서 데이터 받아오기
+    }
+
+    public Weapon GetWeapon(int id)
+    {
+        if ( id < 0 || id >= weapons.Length)
+        {
+            return null;
+        }
+        return weapons[id];
+    }
+
+    public void LevelUpWeapon(int id)
+    {
+        Weapon weapon = GetWeapon(id);
+        if ( weapon != null && weapon.HasWeapon() )
+        {
+            weapon.LevelUP();
+        }
+    }
+
+    public void AddWeaponCount(int id, int count)
+    {
+        Weapon weapon = GetWeapon(id);
+        if (weapon != null)
+        {
+            weapon.AddWeapon(count);
+        }
+    }
+
+}
