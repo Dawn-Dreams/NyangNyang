44746ac4e88ecd368dfc9562ead73f3ae8e35cb1
Lee Yoon Seok <kjggr123@gmail.com>
2024-10-04 00:35:03 +0900

    스프라이트 등의 리소스 에셋을 모든 클래스에 직렬화 시키는 방식이 아닌 Addressable Asset을 통해 Load 및 UnLoad가 되어 메모리 사용을 최적화 할 수 있도록 진행

--- Project/NyangNyang/Assets/Script/UI/Stage/ChangeStage/ThemeButton.cs
+++ Project/NyangNyang/Assets/Script/UI/Stage/ChangeStage/ThemeButton.cs
@@ -3,26 +3,37 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.ResourceManagement.ResourceLocations;
 using UnityEngine.UI;
+
 
 public class ThemeButton : MonoBehaviour
 {
+    // sprite resource
+    private Dictionary<int, Sprite> _stageSprites = null;
+    private AsyncOperationHandle<IList<Sprite>> _spriteHandle;
+
+
     public int startThemeNum = 1;
     private int stageBundle = 5;
-    [SerializeField] private Sprite[] stageSprites;
+    public AssetLabelReference spriteLabel;
     [SerializeField] private Image themeButtonImage;
     [SerializeField] private TextMeshProUGUI themeNameText;
     [SerializeField] private TextMeshProUGUI themeNumberBundleText;
 
-    private void OnValidate()
+    void Awake()
     {
-        if (startThemeNum % 5 != 1 || startThemeNum <= 0)
-        {
-            Debug.LogError("startThemeNum % 5 must 1 and PositiveNumber");
-            return;
-        }
-
+        AssetLoad();
+        stageBundle = GameManager.GetInstance().stageManager.maxStageCount;
+        //stageBundle = 5;
         ChangeButtonStartThemeNum(startThemeNum);
+    }
+
+    private void OnDestroy()
+    {
+        Addressables.Release(_spriteHandle);
     }
 
     public void ChangeButtonStartThemeNum(int newStartThemeNum)
@@ -35,9 +46,24 @@ public class ThemeButton : MonoBehaviour
         startThemeNum = newStartThemeNum;
 
         int themeStep = ((startThemeNum - 1) / stageBundle);
-        Sprite themeBGSprite = stageSprites[themeStep % stageSprites.Length];
+        
+        Sprite themeBGSprite = _stageSprites[themeStep % stageBundle + 1];
         themeButtonImage.sprite = themeBGSprite;
+        
         themeNameText.text = "지역 이름";
         themeNumberBundleText.text = "(" + startThemeNum + " ~ " + (startThemeNum + stageBundle - 1) + ")";
+    }
+
+    void AssetLoad()
+    {
+        _stageSprites = new Dictionary<int, Sprite>();
+
+        _spriteHandle = Addressables.LoadAssetsAsync<Sprite>(spriteLabel, (result) =>
+        {
+            int bgNumber = int.Parse(result.name.Substring(result.name.Length - 1, 1));
+            _stageSprites.Add(bgNumber,result);
+        });
+        _spriteHandle.WaitForCompletion();
+        
     }
 }
