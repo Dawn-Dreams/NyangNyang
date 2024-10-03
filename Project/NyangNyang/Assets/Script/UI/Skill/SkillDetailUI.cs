d18dcb66248690608241d5c59ba54441c7e31567
Gahyun <elfgh29@naver.com>
2024-10-03 22:20:01 +0900

    스킬 시스템 UI 연결

--- Project/NyangNyang/Assets/Script/UI/Skill/SkillDetailUI.cs
+++ Project/NyangNyang/Assets/Script/UI/Skill/SkillDetailUI.cs
@@ -1,18 +1,78 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SkillDetailUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Skill choosedSkill;
+    private SkillMgrUI skillMgrUI;
+
+    public GameObject detailPanel;
+    public GameObject skillPanel;
+    public GameObject effectPanel;
+
+    public Text playerCoinTxt;              //  
+    public Text weaponCoinTxt;
+
+    private Text wNameTxt;
+    private Text wPossessionTxt;
+    private Text wLevelTxt;
+    private Image wImage;
+    private Slider wPossessionSlider;
+
+    private Text eCurStatusTxt;
+    private Text eNextStatusTxt;
+
+    private void Start()
     {
-        
+        skillMgrUI = gameObject.GetComponent<SkillMgrUI>();
+
+        wNameTxt = skillPanel.transform.Find("name_txt").GetComponent<Text>();
+        wPossessionTxt = skillPanel.transform.Find("possession_txt").GetComponent<Text>();
+        wLevelTxt = skillPanel.transform.Find("levelNum_txt").GetComponent<Text>();
+        wImage = skillPanel.transform.Find("weapon_img").GetComponent<Image>();
+        wPossessionSlider = skillPanel.transform.Find("possession_slider").GetComponent<Slider>();
+
+        eCurStatusTxt = effectPanel.transform.Find("prev_txt").GetComponent<Text>();
+        eNextStatusTxt = effectPanel.transform.Find("next_txt").GetComponent<Text>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        // 스킬 인벤 창이 열릴 때 active 됨 디테일 창 X
+        choosedSkill = null;
     }
+
+    public void OnClickedSkill(GameObject _obj)
+    {
+        if ( choosedSkill == null )
+        {
+            detailPanel.SetActive(true);
+
+            choosedSkill = SkillManager.GetInstance().GetSkill(_obj.name);
+
+            wNameTxt.text = choosedSkill.GetName();
+            wLevelTxt.text = choosedSkill.GetLevel() + "/100";
+            wImage.sprite = SkillManager.GetInstance().GetSprite(choosedSkill.GetID());
+
+            int count = choosedSkill.GetPossession();
+            // weaponCoinTxt.text = choosedSkill.GetNeedCoin().ToString();
+            wPossessionTxt.text = count + "/5";
+            wPossessionSlider.value = (float)count / 5 >= 1 ? 1 : (float)count / 5;
+
+            // eCurStatusTxt.text = choosedSkill.GetCurStatus().ToString();
+            // eNextStatusTxt.text = choosedSkill.GetNextStatus().ToString();
+        }
+    }
+
+    public void OnClickedCancle()
+    {
+        if ( choosedSkill != null )
+        {
+            choosedSkill = null;
+            detailPanel.SetActive(false);
+        }
+    }
+
 }
--- Project/NyangNyang/Assets/Script/UI/Skill/SkillMgrUI.cs
+++ Project/NyangNyang/Assets/Script/UI/Skill/SkillMgrUI.cs
@@ -1,18 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SkillMgrUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject[] skills;
+    WeaponUnlock[] skillList = new WeaponUnlock[25];
+    Slider[] sliders = new Slider[25];
+    Text[] texts = new Text[25];
+
+    private void Awake()
     {
-        
+        for ( int i = 0; i < skills.Length; i++)
+        {
+            skillList[i] = skills[i].GetComponent<WeaponUnlock>();
+            sliders[i] = skills[i].transform.Find("Slider").GetComponent<Slider>();
+            texts[i] = skills[i].transform.Find("possession").GetComponent<Text>();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        UpdateAllPossession();
+    }
+
+    public void UpdateAllPossession()
+    {
+        for (int i = 0; i < sliders.Length; i++)
+        {
+            Skill skill = SkillManager.GetInstance().GetSkill(i);
+            if (skill != null)
+            {
+                sliders[i].value = (float)skill.GetPossession() / 5 >= 1 ? 1 : (float)skill.GetPossession() / 5;
+                texts[i].text = skill.GetPossession().ToString() + "/5";
+            }
+        }
+    }
+
+    public void UpdatePossession(int id)
+    {
+        Skill skill = SkillManager.GetInstance().GetSkill(id);
+        if (skill != null)
+        {
+            skillList[id].Unlock();
+            sliders[id].value = (float)skill.GetPossession() / 5 >= 1 ? 1 : (float)skill.GetPossession() / 5;
+            texts[id].text = skill.GetPossession().ToString() + "/5";
+        }
     }
 }
