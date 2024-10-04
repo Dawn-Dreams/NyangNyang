0bb149604ecde581e619aeec9aea4497e2b8dd3f
Gahyun <elfgh29@naver.com>
2024-10-04 12:51:15 +0900

    뽑기 후 뽑힌 객체가 사라지지 않는 오류 수정

--- Project/NyangNyang/Assets/Script/UI/Gacha/PickUpWeapon.cs
+++ Project/NyangNyang/Assets/Script/UI/Gacha/PickUpWeapon.cs
@@ -5,44 +5,36 @@ using UnityEngine.UI;
 
 public class PickUpWeapon : MonoBehaviour
 {
-
-    public ScrollRect ScrollRect;
-    public float space = 10f;
-    public GameObject uiPrefeb;
-    public List<RectTransform> uiObjects = new List<RectTransform>();
     public GameObject AllContent;
+    public GameObject OneContent;
 
-    private void Start()
+    private void OnDisable()
     {
+        AllContent.SetActive(false);
+        OneContent.SetActive(false);
     }
 
     public void ShowPickUpWeapon()
     {
         // 한 개 뽑기
-        var newUI = Instantiate(uiPrefeb, ScrollRect.content);
-        RectTransform newUIRect = newUI.GetComponent<RectTransform>();
+
+        OneContent.SetActive(true);
 
         // TODO: newUI 속의 내용 작성하기 뽑기에서 나온 결과물로**
         int id = 0;
 
-        Image img = newUI.transform.Find("Image").GetComponent<Image>();
+        Image img = OneContent.transform.Find("Image").GetComponent<Image>();
         img.sprite = WeaponManager.GetInstance().GetSprite(id);
-        newUI.GetComponent<WeaponUnlock>().Unlock();
+        OneContent.GetComponent<WeaponUnlock>().Unlock();
         WeaponManager.GetInstance().AddWeaponCount(id, 1);
 
         Weapon weapon = WeaponManager.GetInstance().GetWeapon(id);
 
-        Slider slider = newUI.transform.Find("Slider").GetComponent<Slider>();
+        Slider slider = OneContent.transform.Find("Slider").GetComponent<Slider>();
         slider.value = (float)weapon.GetWeaponCount() / 5 >= 1 ? 1 : (float)weapon.GetWeaponCount() / 5;
 
-        Text text = newUI.transform.Find("possession").GetComponent<Text>();
+        Text text = OneContent.transform.Find("possession").GetComponent<Text>();
         text.text = weapon.GetWeaponCount().ToString() + "/5";
-
-        float contentHeight = ScrollRect.viewport.sizeDelta.y;
-        float uiHeight = newUIRect.sizeDelta.y;
-
-        float centerY = (contentHeight + uiHeight) / 2f;
-        newUIRect.anchoredPosition = new Vector2(0f, -centerY);
     }
 
     public void ShowPickUpWeapons()
