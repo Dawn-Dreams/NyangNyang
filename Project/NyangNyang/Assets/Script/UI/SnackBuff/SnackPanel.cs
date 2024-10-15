435eaffc3e68a96c71d2a337bb0229bb49190430
Lee Yoon Seok <kjggr123@gmail.com>
2024-10-15 23:37:13 +0900

    클라이언트 상에서 간식 버프 버튼 누르면 버프가 10초 적용되고 종료되게 구현 (버프 기능 추후 구현 예정)

--- Project/NyangNyang/Assets/Script/UI/SnackBuff/SnackPanel.cs
+++ Project/NyangNyang/Assets/Script/UI/SnackBuff/SnackPanel.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -13,5 +14,10 @@ public class SnackPanel : MonoBehaviour
 
     public Button showAdButton;
     public GameObject eatingImageObject;
+    public TextMeshProUGUI remainTimeText;
 
+    public void SetRemainTimeText(string newTime)
+    {
+        remainTimeText.text = newTime;
+    }
 }
