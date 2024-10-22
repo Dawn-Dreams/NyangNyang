6ba12b75b10cba4f945a9404349df8c7d57889e8
GH_NoteBook <elfgh29@naver.com>
2024-10-23 08:55:23 +0900

    상점 패널 변경 기능 구현

--- Project/NyangNyang/Assets/Script/UI/Store/StoreManager.cs
+++ Project/NyangNyang/Assets/Script/UI/Store/StoreManager.cs
@@ -4,28 +4,64 @@ using UnityEngine;
 
 public class StoreManager : MonoBehaviour
 {
+    [SerializeField]
+    private List<GameObject> panels = new List<GameObject>();
+
+    private int curPanel = 0;
+
     public void OnClickedSkillGachaNavButton()
     {
         Debug.Log("스킬 뽑기");
+
+        if ( curPanel != 0)
+        {
+            panels[curPanel].SetActive(false);
+            curPanel = 0;
+            panels[curPanel].SetActive(true);
+        }
     }
 
     public void OnClickedWeaponGachaNavButton()
     {
         Debug.Log("무기 뽑기");
+        if (curPanel != 1)
+        {
+            panels[curPanel].SetActive(false);
+            curPanel = 1;
+            panels[curPanel].SetActive(true);
+        }
     }
 
     public void OnClickedGoldNavButton()
     {
         Debug.Log("재화 상점");
+        if (curPanel != 2)
+        {
+            panels[curPanel].SetActive(false);
+            curPanel = 2;
+            panels[curPanel].SetActive(true);
+        }
     }
 
     public void OnClickedPassNavButton()
     {
         Debug.Log("패스 상점");
+        if (curPanel != 3)
+        {
+            panels[curPanel].SetActive(false);
+            curPanel = 3;
+            panels[curPanel].SetActive(true);
+        }
     }
 
     public void OnClickedPackageNavButton()
     {
         Debug.Log("패키지 상점");
+        if (curPanel != 4)
+        {
+            panels[curPanel].SetActive(false);
+            curPanel = 4;
+            panels[curPanel].SetActive(true);
+        }
     }
 }
