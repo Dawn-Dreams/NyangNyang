bb84bd9bbb54262d7d489a90c02326219c5194c1
GH_NoteBook <elfgh29@naver.com>
2024-11-13 18:30:23 +0900

    장비 장착 구현을 위한 스크립트 생성

---
+++ Project/NyangNyang/Assets/Script/Character/EquippedItem.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquippedItem : MonoBehaviour
+{
+    Weapon EquippedWeapon;
+    List<Skill> EquippedSkills;
+
+    public void OnClickedEquippedButton()
+    {
+        // 장착 디테일 창 보여주기
+    }
+
+
+}
