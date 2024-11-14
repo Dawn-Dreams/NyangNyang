4033e5d167228da47f5cf37ab51ff8418ef86a22
Gahyun <elfgh29@naver.com>
2024-11-14 09:55:10 +0900

    장착된 장비 변경 기능 구현 중

--- Project/NyangNyang/Assets/Script/Character/EquippedItem.cs
+++ Project/NyangNyang/Assets/Script/Character/EquippedItem.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EquippedItem : MonoBehaviour
 {
@@ -8,22 +9,34 @@ public class EquippedItem : MonoBehaviour
     Weapon SelectedWeapon;
     List<Skill> EquippedSkills;
 
+    [SerializeField]
+    Image WeaponImage;
+
     public void OnClickedWeaponEquippedButton()
     {
         if (SelectedWeapon != null)
         {
             EquippedWeapon = SelectedWeapon;
+            UpdateEquippedWeapon();
         }
     }
 
     public void OnClickedWeapon(GameObject _obj)
     {
-        SelectedWeapon = WeaponManager.GetInstance().GetWeapon(_obj.name);
+        if ( _obj != null)
+        {
+            SelectedWeapon = WeaponManager.GetInstance().GetWeapon(_obj.name);
+        }
     }
 
     void UpdateEquippedWeapon()
     {
+        int id = SelectedWeapon.GetID();
+        WeaponImage.sprite = WeaponManager.GetInstance().GetSprite(id);
 
+        /*
+            공격력 관련 코드 작성         
+         */
     }
 
 }
