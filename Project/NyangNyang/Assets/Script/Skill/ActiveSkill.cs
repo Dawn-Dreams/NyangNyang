c30164ab2f0dc07b794aab1aa560246406f571a9
GH_NoteBook <elfgh29@naver.com>
2024-11-24 17:27:11 +0900

    스크립트 오류 수정

--- Project/NyangNyang/Assets/Script/Skill/ActiveSkill.cs
+++ Project/NyangNyang/Assets/Script/Skill/ActiveSkill.cs
@@ -2,8 +2,8 @@ public abstract class ActiveSkill : Skill
 {
     float coolTime;
 
-    public ActiveSkill(int _id, string _name, int _possession, int _level, int _levelUpCost)
-    : base(_id, _name, _possession, _level, _levelUpCost)
+    public ActiveSkill(int _id, string _name, int _possession, int _level, int _levelUpCost, int _effect)
+    : base(_id, _name, _possession, _level, _levelUpCost, _effect)
     {
     }
 
